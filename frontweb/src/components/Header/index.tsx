567e87948dd2a56fb163c80c6f04370f16a2fcc1
FrankDestro <frank-destro@outlook.com>
2023-01-31 23:12:51 -0300

    Ajustado navbar e sidebar

--- frontweb/src/components/Header/index.tsx
+++ frontweb/src/components/Header/index.tsx
@@ -4,63 +4,87 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import LogoMain from '../../assets/img/logomain.png';
 import { ReactComponent as Notification } from 'assets/img/notification.svg';
 import { ReactComponent as Resources } from 'assets/img/resources.svg';
+import { ReactComponent as Logo } from 'assets/img/logomain.svg';
 
 import './styles.css';
 
 function Header() {
-    return (
-        <>
-            <Navbar collapseOnSelect expand="lg" bg="white" className='navbar' >
-                <Container>
-                    <div className='logomain-container'>
-                        <img src={LogoMain} alt="Speedometer" />
-                    </div>
-                    <Navbar.Brand href="#home">Sistema</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                        </Nav>
-                        <Nav>
-                        <div className="menu-content">
-                          <span>administrador@grbsf.com.br</span>
-                            <NavDropdown title={
-                                <Resources />
-                            } id="collasible-nav-dropdown">
-                                  
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                            <div className='notification-container'>
-                                <NavDropdown title={
-                                    <><><Notification /></><span className="label label-danger">3</span></>
-                                } id="collasible-nav-dropdown">
-                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                </NavDropdown>
-                            </div>
-                            </div>
-
-
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-
-
-
-        </>
-    );
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="white" className="navbar">
+        <div className="container-fluid">
+          <div className="container-logo">
+            <Logo></Logo>
+            <h4>GRB Dashboard bot</h4>
+          </div>
+        </div>
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto"></Nav>
+            <Nav>
+              <div className="menu-content">
+                <span>administrador@grbsf.com.br</span>
+                <NavDropdown title={<Resources />} id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <div className="notification-container">
+                  <NavDropdown
+                    title={
+                      <>
+                        <>
+                          <Notification />
+                        </>
+                        <span className="label label-danger">3</span>
+                      </>
+                    }
+                    id="collasible-nav-dropdown"
+                  >
+                    <NavDropdown.Item href="#action/3.1">
+                      Action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">
+                      Something
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.4">
+                      Separated link
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.1">
+                      Action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.1">
+                      Action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.1">
+                      Action
+                    </NavDropdown.Item>
+                     <NavDropdown.Item href="#action/3.1">
+                      Action
+                    </NavDropdown.Item>
+                    
+                  </NavDropdown>
+                </div>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default Header;
--- frontweb/src/components/Sidebar/index.tsx
+++ frontweb/src/components/Sidebar/index.tsx
@@ -1,30 +1,38 @@
-
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import { Menu, MenuItem, Sidebar, SubMenu, useProSidebar } from 'react-pro-sidebar';
 import collapse from '../../assets/img/collapseicon.png';
+import { ReactComponent as Box } from 'assets/img/box.svg';
+import { ReactComponent as Bag } from 'assets/img/bag.svg';
+import { ReactComponent as Meta } from 'assets/img/meta.svg';
+import { ReactComponent as Hide } from 'assets/img/expand.svg';
 
 import './styles.css';
-
 
 function Side() {
 
   const { collapseSidebar } = useProSidebar();
 
   return (
+    <>
     <div className='sidebar-container'>
       <Sidebar transitionDuration={1000}>
         <Menu>
           <Menu>
-            <SubMenu label="Charts">
-              <MenuItem> Pie charts</MenuItem>
+            <SubMenu label="Charts" icon={<Box/>}> 
+              <MenuItem>  Pie charts</MenuItem>
               <MenuItem> Line charts</MenuItem>
               <MenuItem> Bar charts</MenuItem>
             </SubMenu>
-            <SubMenu label="Maps">
+            <SubMenu label="Charts" icon={<Box/>}> 
+              <MenuItem>  Pie charts</MenuItem>
+              <MenuItem> Line charts</MenuItem>
+              <MenuItem> Bar charts</MenuItem>
+            </SubMenu>
+            <SubMenu label="Maps" icon={<Bag/>}>
               <MenuItem> Google maps</MenuItem>
               <MenuItem> Open street maps</MenuItem>
             </SubMenu>
-            <SubMenu label="Theme">
+            <SubMenu label="Theme" icon={<Meta/>}>
               <MenuItem> Dark</MenuItem>
               <MenuItem> Light</MenuItem>
             </SubMenu>
@@ -33,16 +41,12 @@ function Side() {
       </Sidebar>
       <main style={{ padding: 10 }}>
         <div>
-          <img
-            src={collapse}
-            alt="Profile"
-            onClick={() => collapseSidebar()}
-            id="logout-img"
-            className="image-expand brightness blur saturate grayscale contrast"
-          />
+        <Hide  onClick={() => collapseSidebar()} cursor="pointer" />
         </div>
+      
       </main>
     </div>
+    </>
   )
 }
 
